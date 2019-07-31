91813c6c2c0e3fc9ed1ececc9364935480e1d033
labue20 <labuewilfred@gmail.com>
2019-07-30 23:48:48 -0500

    Update Mapper03.java

--- Mapper03.java
+++ Mapper03.java
@@ -6,16 +6,17 @@ public class Mapper03 {
 
     public static void Mapper(String stemWords) {
 
+        //check if the stemWords is null , if not strim the strings and append 1 to each word
         if (stemWords != null) {
 
-            String[] words = stemWords.split(" ");
-
+            String[] words = stemWords.split(" ");//split the strings and remove spaces
+            //appends a 1 to each word
             for (int i = 0; i < words.length; i++) {
                 words[i] = words[i] + " 1";
             }
 
             for (int i = 0; i < words.length; i++) {
-                System.out.print(words[i] + ",");
+                System.out.print(words[i] + ",");//prints the output to console
 
             }
 
@@ -24,9 +25,10 @@ public class Mapper03 {
 
     public static void main(String[] args) throws IOException {
 
+        //reads standard input using inputStream Reader 
         Reader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
-        String x = br.readLine();
-        Mapper(x);
+        String x = br.readLine(); 
+        Mapper(x); //calls the mapper method
     }
 }
